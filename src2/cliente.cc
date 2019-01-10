5dafc429d1ff9264d1ba7be5830d7bca8a4bf714
ClaudioRojas98 <44007979+ClaudioRojas98@users.noreply.github.com>
2019-01-10 10:38:54 -0500

    Update cliente.cc

--- src2/cliente.cc
+++ src2/cliente.cc
@@ -17,37 +17,29 @@ int main(int argc, char **argv){
     printf("<host> <puerto>\n");
     return 1;
   }
-  struct sockaddr_in cliente; //Declaración de la estructura con información para la conexión
-  struct hostent *servidor; //Declaración de la estructura con información del host
-  servidor = gethostbyname(argv[1]); //Asignacion
+  struct sockaddr_in cliente; //Se declara la conexion
+  struct hostent *servidor; //se declara la informacion del host
+  servidor = gethostbyname(argv[1]); 
   if(servidor == NULL)
-  { //Comprobación
+  { // Se comprueba que exista conexion
     printf("Host erróneo\n");
     return 1;
   }
   int puerto, conexion;
   char buffer[100];
   conexion = socket(AF_INET, SOCK_STREAM, 0); //Asignación del socket
-  puerto=(atoi(argv[2])); //conversion del argumento
-  bzero((char *)&cliente, sizeof((char *)&cliente)); //Rellena toda la estructura de 0's
-        //La función bzero() es como memset() pero inicializando a 0 todas la variables
-  cliente.sin_family = AF_INET; //asignacion del protocolo
-  cliente.sin_port = htons(puerto); //asignacion del puerto
+  puerto=(atoi(argv[2])); //Conversion del argumento
+  bzero((char *)&cliente, sizeof((char *)&cliente));
+  cliente.sin_family = AF_INET;
+  cliente.sin_port = htons(puerto); 
   bcopy((char *)servidor->h_addr, (char *)&cliente.sin_addr.s_addr, sizeof(servidor->h_length));
-  //bcopy(); copia los datos del primer elemendo en el segundo con el tamaño máximo 
-  //del tercer argumento.
-
-
-  //cliente.sin_addr = *((struct in_addr *)servidor->h_addr); //<--para empezar prefiero que se usen
-  //inet_aton(argv[1],&cliente.sin_addr); //<--alguna de estas dos funciones
   if(connect(conexion,(struct sockaddr *)&cliente, sizeof(cliente)) < 0)
-  { //conectando con el host
+  { //Se conecta con el host
     printf("Error conectando con el host\n");
     close(conexion);
     return 1;
   }
   printf("Conectado con %s:%d\n",inet_ntoa(cliente.sin_addr),htons(cliente.sin_port));
-  //inet_ntoa(); está definida en <arpa/inet.h>
   char respuesta[100];
   printf("1.recibir un consejo\n");
   fgets(respuesta,100,stdin);
@@ -55,9 +47,9 @@ int main(int argc, char **argv){
     string hola = "consejo";
     strcpy(buffer,hola.c_str());
   }
-  send(conexion, buffer, 100, 0); //envio
+  send(conexion, buffer, 100, 0); //Se envia
   bzero(buffer, 100);
-  recv(conexion, buffer, 100, 0); //recepción
+  recv(conexion, buffer, 100, 0); //Se recepciona
   printf("%s\n", buffer);
 return 0;
 }
