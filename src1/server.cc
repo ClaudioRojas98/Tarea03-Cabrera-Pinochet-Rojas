f6295df152c97d2c30801346e9f9b381c89171fa
ClaudioRojas98 <44007979+ClaudioRojas98@users.noreply.github.com>
2019-01-10 10:45:45 -0500

    Update server.cc

--- src1/server.cc
+++ src1/server.cc
@@ -9,27 +9,26 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-#include <vector>  //for std::vector
+#include <vector>
 #include<time.h>
-
 string consejoaleatorio(){
     ifstream ficheroEntrada;
 string frase;
-ficheroEntrada.open ("consejos.txt");
+ficheroEntrada.open ("consejos.txt");//Se abre el archivo con los consejos
 getline(ficheroEntrada,frase);
 srand(time(NULL));
 std::vector<std::string> data;
 int cont=0;
 int num;
-while (frase!=""){
+while (frase!=""){//mientras existan caracteres se sigue contando y se guarda
     data.push_back(frase);
     getline(ficheroEntrada,frase);
     cont++;
 }
 ficheroEntrada.close();
-num = rand()%cont;
+num = rand()%cont;//se genera el consejo random 
 
-    return data[num];
+    return data[num];//posicion del consejo random que se mandara al cliente
 
 }
 
@@ -37,42 +36,42 @@ int main(int argc, char **argv){
     
 
   if(argc<2)
-  { //Especifica los argumentos
+  { //Se piden los argumentos para inicializar la conexion
     printf("%s <puerto>\n",argv[0]);
     return 1;
   }
-  int conexion_servidor, conexion_cliente, puerto; //declaramos las variables
+  int c_servidor, c_cliente, puerto; //Se declaran variables
   socklen_t longc; //Debemos declarar una variable que contendrá la longitud de la estructura
   struct sockaddr_in servidor, cliente;
   char buffer[100]; //Declaramos una variable que contendrá los mensajes que recibamos
   puerto = atoi(argv[1]);
-  conexion_servidor = socket(AF_INET, SOCK_STREAM, 0); //creamos el socket
+  c_servidor = socket(AF_INET, SOCK_STREAM, 0); //creamos el socket
   bzero((char *)&servidor, sizeof(servidor)); //llenamos la estructura de 0's
   servidor.sin_family = AF_INET; //asignamos a la estructura
   servidor.sin_port = htons(puerto);
   servidor.sin_addr.s_addr = INADDR_ANY; //esta macro especifica nuestra dirección
-  if(bind(conexion_servidor, (struct sockaddr *)&servidor, sizeof(servidor)) < 0)
+  if(bind(c_servidor, (struct sockaddr *)&servidor, sizeof(servidor)) < 0)
   { //asignamos un puerto al socket
     printf("Error al asociar el puerto a la conexion\n");
-    close(conexion_servidor);
+    close(c_servidor);
     return 1;
   }
-  listen(conexion_servidor, 3); //Estamos a la escucha
+  listen(c_servidor, 3); //Estamos a la escucha
   printf("A la escucha en el puerto %d\n", ntohs(servidor.sin_port));
   longc = sizeof(cliente); //Asignamos el tamaño de la estructura a esta variable
-  conexion_cliente = accept(conexion_servidor, (struct sockaddr *)&cliente, &longc); //Esperamos una conexion
-  if(conexion_cliente<0)
+  c_cliente = accept(c_servidor, (struct sockaddr *)&cliente, &longc); //Esperamos una conexion
+  if(c_cliente<0)
   {
     printf("Error al aceptar trafico\n");
-    close(conexion_servidor);
+    close(c_servidor);
     return 1;
   }
   printf("Conectando con %s:%d\n", inet_ntoa(cliente.sin_addr),htons(cliente.sin_port));
-  if(recv(conexion_cliente, buffer, 100, 0) < 0)
+  if(recv(c_cliente, buffer, 100, 0) < 0)
   { //Comenzamos a recibir datos del cliente
     //Si recv() recibe 0 el cliente ha cerrado la conexion. Si es menor que 0 ha habido algún error.
     printf("Error al recibir los datos\n");
-    close(conexion_servidor);
+    close(c_servidor);
     return 1;
   }
   else
@@ -82,10 +81,10 @@ int main(int argc, char **argv){
         char consejito[100];
         strcpy(consejito,consejoaleatorio().c_str());
         printf("se envio el consejo %s\n",consejito);
-        send(conexion_cliente,consejito, 100, 0);
+        send(c_cliente,consejito, 100, 0);
     }
     
   }
-  close(conexion_servidor);
+  close(c_servidor);
   return 0;
 }
